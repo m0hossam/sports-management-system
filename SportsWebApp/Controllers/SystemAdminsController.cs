64b789c3ee01498718e6c0b317b91875647efb7d
m0hossam <mohossam1326@gmail.com>
2023-10-14 01:34:06 +0300

    Re-implemented SystemAdminsController actions & views

--- SportsWebApp/Controllers/SystemAdminsController.cs
+++ SportsWebApp/Controllers/SystemAdminsController.cs
@@ -41,6 +41,74 @@ namespace SportsWebApp.Controllers
             return View(systemAdmin);
         }
 
+        // GET: SystemAdmins/Clubs
+        public async Task<IActionResult> Clubs()
+        {
+            return _context.Clubs != null ?
+            View(await _context.Clubs.ToListAsync()) :
+            Problem("Entity set 'ApplicationDbContext.Clubs'  is null.");
+        }
+
+        // GET: SystemAdmins/DeleteClub/
+        public async Task<IActionResult> DeleteClub(int? id)
+        {
+            if (id == null || _context.Clubs == null)
+            {
+                return NotFound();
+            }
+
+            var club = await _context.Clubs.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            return View(club);
+        }
+
+        // POST: SystemAdmins/DeleteClubConfirmed/
+        [HttpPost, ActionName(nameof(DeleteClub))]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteClubConfirmed(int id)
+        {
+            if (_context.Clubs == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Clubs'  is null.");
+            }
+            var club = await _context.Clubs.FindAsync(id);
+            if (club != null)
+            {
+                // Delete club representative associated with club
+                var clubRep = _context.ClubRepresentatives.Include(x => x.User).Include(x => x.Club).FirstOrDefault(x => x.Club == club);
+
+                if (clubRep != null)
+                {
+                    var user = clubRep.User;
+
+                    var result = await _userManager.DeleteAsync(user);
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    if (!result.Succeeded)
+                    {
+                        throw new InvalidOperationException($"Unexpected error occurred deleting user.");
+                    }
+
+                    _logger.LogInformation("User with ID '{UserId}' was deleted because they represent a deleted club entity.", userId);
+                }
+
+                // Delete matches in which club participates/participated
+                var matches = _context.Matches.Where(x => x.HomeClubId == club.Id || x.AwayClubId == club.Id);
+                _context.Matches.RemoveRange(matches);
+
+                // Delete club
+                _context.Clubs.Remove(club);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Clubs));
+
+        }
+
         // GET: SystemAdmins/AddClub
         public IActionResult AddClub()
         {
@@ -48,8 +116,6 @@ namespace SportsWebApp.Controllers
         }
 
         // POST: SystemAdmins/AddClub
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddClub([Bind("Id,Name,Location")] Club club)
@@ -65,72 +131,63 @@ namespace SportsWebApp.Controllers
 
                 _context.Add(club);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(SuccessfulOperation), new { msg = $"Club '{club.Name}' was created successfully." });
+                return RedirectToAction(nameof(Clubs));
             }
             return View(club);
         }
 
-        // GET: SystemAdmins/DeleteClub
-        public IActionResult DeleteClub()
+        // GET: SystemAdmins/Stadiums
+        public async Task<IActionResult> Stadiums()
         {
-            return View();
+            return _context.Stadiums != null ?
+            View(await _context.Stadiums.ToListAsync()) :
+            Problem("Entity set 'ApplicationDbContext.Stadiums'  is null.");
         }
 
-        // POST: SystemAdmins/DeleteClub
-        [HttpPost]
+        // GET: SystemAdmins/DeleteStadium/
+        public async Task<IActionResult> DeleteStadium(int? id)
+        {
+            if (id == null || _context.Stadiums == null)
+            {
+                return NotFound();
+            }
+
+            var stadium = await _context.Stadiums.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (stadium == null)
+            {
+                return NotFound();
+            }
+
+            return View(stadium);
+        }
+
+        // POST: SystemAdmins/DeleteStadiumConfirmed/
+        [HttpPost, ActionName(nameof(DeleteStadium))]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteClub([Bind("Name")] Club wantedClub)
+        public async Task<IActionResult> DeleteStadiumConfirmed(int id)
         {
-            if (_context.Clubs == null)
+            if (_context.Stadiums == null)
             {
-                return Problem("Entity set 'SportsWebAppContext.Clubs'  is null.");
+                return Problem("Entity set 'ApplicationDbContext.Stadiums'  is null.");
+            }
+            var stadium = await _context.Stadiums.FindAsync(id);
+            if (stadium != null)
+            {
+                _context.Stadiums.Remove(stadium);
             }
 
-            var club = _context.Clubs.FirstOrDefault(x => x.Name == wantedClub.Name);
-            if (club == null)
-            {
-                ModelState.AddModelError(string.Empty, $"There exists no club with the name of {wantedClub.Name}.");
-                return View(wantedClub);
-            }
-
-            // Delete club representative associated with club
-            var clubRep = _context.ClubRepresentatives.Include(x => x.User).Include(x => x.Club).FirstOrDefault(x => x.Club == club);
-
-            if (clubRep != null)
-            {
-                var user = clubRep.User;
-
-                var result = await _userManager.DeleteAsync(user);
-                var userId = await _userManager.GetUserIdAsync(user);
-                if (!result.Succeeded)
-                {
-                    throw new InvalidOperationException($"Unexpected error occurred deleting user.");
-                }
-
-                _logger.LogInformation("User with ID '{UserId}' was deleted because they represent a deleted club entity.", userId);
-            }
-
-            // Remove matches in which club participates/participated
-            var matches = _context.Matches.Where(x => x.HomeClub == club || x.AwayClub == club);
-            _context.Matches.RemoveRange(matches);
-
-            _context.Clubs.Remove(club);
             await _context.SaveChangesAsync();
-
-            return RedirectToAction(nameof(SuccessfulOperation), new { msg = $"Club '{club.Name}' was deleted successfully." });
+            return RedirectToAction(nameof(Stadiums));
         }
-
 
         // GET: SystemAdmins/AddStadium
-       
         public IActionResult AddStadium()
         {
             return View();
         }
 
         // POST: SystemAdmins/AddStadium
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddStadium([Bind("Id,Name,Location,Capacity")] Stadium stadium)
@@ -152,124 +209,74 @@ namespace SportsWebApp.Controllers
 
                 _context.Add(stadium);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(SuccessfulOperation), new { msg = $"Stadium '{stadium.Name}' was created successfully." });
+                return RedirectToAction(nameof(Stadiums));
             }
             return View(stadium);
         }
 
-        //
-        /// <summary>
-        /// </summary>
-        /// <returns></returns>
-        // GET: SystemAdmins/DeleteStadium
-        public IActionResult DeleteStadium()
+        // GET: SystemAdmins/Fans
+        public async Task<IActionResult> Fans()
         {
-            return View();
+            return _context.Fans != null ?
+            View(await _context.Fans.ToListAsync()) :
+            Problem("Entity set 'ApplicationDbContext.Fans'  is null.");
         }
 
-        // POST: SystemAdmins/DeleteStadium
+        // GET: SystemAdmins/FanDetails/
+        public async Task<IActionResult> FanDetails(int? id)
+        {
+            if (id == null || _context.Fans == null)
+            {
+                return NotFound();
+            }
+
+            var fan = await _context.Fans.FirstOrDefaultAsync(f => f.Id == id);
+
+            if (fan == null)
+            {
+                return NotFound();
+            }
+
+            return View(fan);
+        }
+
+        // POST: SystemAdmins/BlockFan/
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteStadium([Bind("Name")] Stadium wantedStadium)
-        {
-            if (_context.Stadiums == null)
-            {
-                return Problem("Entity set 'SportsWebAppContext.Stadiums'  is null.");
-            }
-
-            var stadium = _context.Stadiums.FirstOrDefault(x => x.Name == wantedStadium.Name);
-            if (stadium == null)
-            {
-                ModelState.AddModelError(string.Empty, $"There exists no stadium with the name of {wantedStadium.Name}.");
-                return View(wantedStadium);
-            }
-
-            // Delete stadium manager associated with stadium
-            var stadiumManager = _context.StadiumManagers.Include(x => x.User).Include(x => x.Stadium).FirstOrDefault(x => x.Stadium == stadium);
-
-            if (stadiumManager != null)
-            {
-                var user = stadiumManager.User;
-
-                var result = await _userManager.DeleteAsync(user);
-                var userId = await _userManager.GetUserIdAsync(user);
-                if (!result.Succeeded)
-                {
-                    throw new InvalidOperationException($"Unexpected error occurred deleting user.");
-                }
-
-                _logger.LogInformation("User with ID '{UserId}' was deleted because they manage a deleted stadium entity.", userId);
-            }
-
-            _context.Stadiums.Remove(stadium);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction(nameof(SuccessfulOperation), new { msg = $"Stadium '{stadium.Name}' was deleted successfully." });
-        }
-
-        // GET: SystemAdmins/BlockFan
-        public IActionResult BlockFan()
-        {
-            return View();
-        }
-
-        // POST: SystemAdmins/BlockFan
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> BlockFan([Bind("NationalId")] Fan wantedFan)
+        public async Task<IActionResult> BlockFan(int id)
         {
             if (_context.Fans == null)
             {
-                return Problem("Entity set 'SportsWebAppContext.Fans'  is null.");
+                return Problem("Entity set 'ApplicationDbContext.Fans'  is null.");
             }
-
-            var fan = _context.Fans.FirstOrDefault(x => x.NationalId == wantedFan.NationalId);
-            if (fan == null)
+            var fan = await _context.Fans.FindAsync(id);
+            if (fan != null)
             {
-                ModelState.AddModelError(string.Empty, $"There exists no fan with a national ID '{wantedFan.NationalId}'.");
-                return View(wantedFan);
+                fan.IsBlocked = true;
             }
 
-            fan.IsBlocked = true;
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(SuccessfulOperation), new { msg = $"The fan with the national ID of '{fan.NationalId}' was successfully blocked." });
+            return RedirectToAction(nameof(Fans));
         }
 
-        // GET: SystemAdmins/UnblockFan
-        public IActionResult UnblockFan()
-        {
-            return View();
-        }
-
-        //Post: SystemAdmins/UnblockFan
+        // POST: SystemAdmins/UnblockFan/
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> UnblockFan([Bind("NationalId")] Fan wantedFan)
+        public async Task<IActionResult> UnblockFan(int id)
         {
             if (_context.Fans == null)
             {
-                return Problem("Entity set 'SportsWebAppContext.Fans'  is null.");
+                return Problem("Entity set 'ApplicationDbContext.Fans'  is null.");
             }
-            var fan = _context.Fans.FirstOrDefault(x => x.NationalId == wantedFan.NationalId);
-            if (fan == null)
+            var fan = await _context.Fans.FindAsync(id);
+            if (fan != null)
             {
-                ModelState.AddModelError(string.Empty, $"There exists no fan with a national ID '{wantedFan.NationalId}'.");
-                return View(wantedFan);
+                fan.IsBlocked = false;
             }
 
-            fan.IsBlocked = false;
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(SuccessfulOperation), new { msg = $"The fan with the national ID of '{fan.NationalId}' was successfully unblocked." });
-
+            return RedirectToAction(nameof(Fans));
         }
 
-
-
-        // GET: SystemAdmins/SuccessfulOperation
-        public IActionResult SuccessfulOperation(string? msg)
-        {
-            TempData["Success Message"] = (msg != null) ? msg : "";
-            return View();
-        }
     }
 }
